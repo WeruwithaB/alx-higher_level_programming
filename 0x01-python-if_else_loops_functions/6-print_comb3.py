3a326c9fe116b54e18f1b786d55b38b2ce31d236
WeruwithaB <brandonwaweru24@gmail.com>
2022-11-29 23:18:08 -0800

    Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Auhtor - Brandon Waweru
+
+for digit1 in range(0, 10):
+    for digit 2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+This is my project task work. 0x01. Python - if/else, loops, functions.
